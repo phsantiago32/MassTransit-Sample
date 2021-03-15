5e3f5514527e49e25f3e1d89b6101c25e1176bdf
Pedro Santiago <phserafim@eservicios.indracompany.com>
2021-03-15 18:42:06 -0300

    add project files

---
+++ src/Consumer/OrderConsumer.cs
@@ -0,0 +1,35 @@
+﻿using MassTransit;
+using Microsoft.Extensions.Logging;
+using Models;
+using System;
+using System.Threading.Tasks;
+
+namespace Consumer
+{
+    public sealed class OrderConsumer : IConsumer<Order>
+    {
+        private readonly ILogger<OrderConsumer> _logger;
+
+        public OrderConsumer(ILogger<OrderConsumer> logger)
+        {
+            _logger = logger;
+        }
+
+        public Task Consume(ConsumeContext<Order> context)
+        {
+            try
+            {
+                var order = context.Message;
+
+                _logger.LogInformation($"Order received: {order.OrderId} - {DateTime.Now:o}");
+
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError("Error on try to consume order", ex);
+            }
+
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ src/Publisher/Controllers/OrderController.cs
@@ -0,0 +1,36 @@
+﻿using MassTransit;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Models;
+using System;
+using System.Threading.Tasks;
+
+namespace Publisher.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrderController : ControllerBase
+    {
+        private const string QueueUri = "rabbitmq://localhost/order-consumer-queue";
+        private readonly ILogger<OrderController> _logger;
+        private readonly IBusControl _busControl;
+
+        public OrderController(ILogger<OrderController> logger, IBusControl busControl)
+        {
+            _logger = logger;
+            _busControl = busControl;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] long orderId)
+        {
+            var endpoint = await _busControl.GetSendEndpoint(new Uri(QueueUri));
+
+            await endpoint.Send(new Order { OrderId = orderId });
+
+            _logger.LogInformation($"Message published. OrderId: {orderId}");
+
+            return Ok($"{DateTime.Now:o}");
+        }
+    }
+}
